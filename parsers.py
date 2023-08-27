a8ada7969773165750fb4bebea430abbfc3b7b3e
Roman <75179549+ramankutuzau@users.noreply.github.com>
2023-08-27 03:21:30 +0300

    create project

---
+++ parsers.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+
+
+def parser_chats():
+    with open('chats.html', 'r', encoding='utf-8') as f:
+        html = f.read()
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    ul_element = soup.find('ul')
+
+    chat_data_list = []
+
+    for li_element in ul_element.find_all('li', class_='styles_menu-conversation-item__xRVEN'):
+        conversation_id = li_element.find('div', class_='styles_sides-block__QJXBM')['data-conversation-id']
+
+        sender_name_element = li_element.find('p', class_='styles_right-side__top-name__520T1')
+        sender_name = sender_name_element.text.strip() if sender_name_element else ""
+
+        timestamp_element = li_element.find('span', class_='styles_right-side__top-time__lcbOS')
+        timestamp = timestamp_element.text.strip() if timestamp_element else ""
+
+        message_element = li_element.find('span', class_='styles_preview__v8Q7r')
+        message = message_element.text.strip() if message_element else ""
+
+        unseen_messages_element = li_element.find('span', class_='styles_unseen-message-badge__mU2mu')
+        unseen_messages = unseen_messages_element.text.strip() if unseen_messages_element else ""
+
+        chat_data = {
+            'chat_id': conversation_id,
+            'sender_name': sender_name,
+            'timestamp': timestamp,
+            'message': message,
+            'unseen_messages': unseen_messages
+        }
+        chat_data_list.append(chat_data)
+
+    return chat_data_list
+
+
+# parsed_chats = parser_chats()
+# for chat in parsed_chats:
+#     print(chat)
---
+++ selenium_bot.py
@@ -0,0 +1,170 @@
+import os
+import time
+
+from selenium import webdriver
+from selenium.common import TimeoutException
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from parsers import parser_chats
+
+
+import pickle
+from fake_useragent import UserAgent
+
+import parsers
+
+
+def accept_cookie(driver):
+    accept_button = driver.find_element(By.XPATH, '//button[text()="Принять"]')
+    if accept_button:
+        accept_button.click()
+        print("accept cookie")
+
+def save_cookie(driver):
+    with open('cookies.pickle', 'wb') as f:
+        pickle.dump(driver.get_cookies(), f)
+
+def load_cookie(driver):
+    with open('cookies.pickle', 'rb') as f:
+        for cookie in pickle.load(f):
+            driver.add_cookie(cookie)
+    print('load cookie')
+
+def login(driver, username, password):
+    try:
+        wait = WebDriverWait(driver, 10)
+
+        login_input = wait.until(EC.presence_of_element_located((By.NAME, "login")))
+        login_input.clear()
+        login_input.send_keys(username)
+        print('Entered username')
+
+        password_input = wait.until(EC.presence_of_element_located((By.NAME, "password")))
+        password_input.clear()
+        password_input.send_keys(password)
+        print('Entered password')
+
+        password_input.send_keys(Keys.ENTER)
+        print('Logging in...')
+
+    except TimeoutException:
+        print("Element did not show up")
+
+def get_all_chats(driver):
+    print('go to /messaging/')
+    driver.get("https://www.kufar.by/account/messaging/")
+    print('get and sleep 3 sec')
+
+    time.sleep(3)
+    # Прокрутите страницу до элемента
+    try:
+        # Найти элемент ul внутри styles_menu-conversations-list__n7m8b
+        chats_element = driver.find_element(By.CSS_SELECTOR, ".styles_menu-conversations-list__n7m8b ul")
+
+        # Получаем высоту элемента ul
+        ul_height = driver.execute_script("return arguments[0].scrollHeight", chats_element)
+        # print(f'ul height {ul_height}')
+        # Прокручиваем до конца
+        while True:
+            # Прокручиваем элемент ul
+            driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", chats_element)
+
+            # Подождать некоторое время, чтобы содержимое успело подгрузиться
+            time.sleep(2)
+            print('scroll chats for load')
+            # Получаем новую высоту элемента ul после прокрутки
+            new_ul_height = driver.execute_script("return arguments[0].scrollHeight", chats_element)
+            # print(f'new ul height {new_ul_height}')
+
+            # Если высота не изменилась, значит мы достигли конца списка
+            if new_ul_height == ul_height:
+                break
+                print('scroll end...')
+
+            # Обновляем высоту элемента ul
+            ul_height = new_ul_height
+        time.sleep(3)
+    finally:
+        # Закрыть браузер после выполнения
+        pass
+    # Прокрутка страницы
+    chats = driver.find_element(By.XPATH, '//div[@class="styles_menu-conversations-list__n7m8b"]')
+    chats_html = chats.get_attribute("outerHTML")
+    # Сохраните содержимое страницы в файл
+
+    with open('chats.html', 'w', encoding='utf-8') as f:
+        f.write(str(chats_html))
+    parser_chats()
+    print('chats_save')
+
+    # for el in elements:
+    #     print(f"chat id : {el.get_attribute('data-conversation-id')}")
+    #
+    # chat_id = elements[2].get_attribute('data-conversation-id')
+    #
+
+
+def main():
+    url = 'https://www.kufar.by/login'
+    user_agent = UserAgent()
+    print(user_agent.random)
+    chrome_options = webdriver.ChromeOptions()
+    # chrome_options.page_load_strategy = 'normal'
+    # chrome_options.add_argument('--disable-gpu')
+    # chrome_options.add_argument('--headless')
+    # chrome_options.add_argument(f"--user-agent={user_agent.random}")
+
+
+    driver = webdriver.Chrome(options=chrome_options)
+    print('start...')
+    driver.get(url)
+
+    time.sleep(2)
+    #
+    accept_cookie(driver)
+
+    if not os.path.exists("cookies.pickle"):
+        print('cookie is not exists')
+        username = ""
+        password = ""
+        login(driver=driver,username=username,password=password)
+        driver.refresh()
+        time.sleep(3)
+        save_cookie(driver)
+    else:
+        print('cookie is exists')
+        # Если файл cookie есть, загружаем его
+        load_cookie(driver)
+        print('reload page')
+        driver.get(url)
+
+
+
+    get_all_chats(driver)
+
+    # print('accept cookie')
+
+
+
+    # print('sleep 5 sec')
+    # driver.get(f"https://www.kufar.by/account/messaging/{chat_id}")
+    # time.sleep(5)
+    # textarea = driver.find_element(By.XPATH,'//textarea[@name="message_textarea"]')
+    # textarea.clear()
+    # textarea.send_keys('!!!')
+    # textarea.send_keys(Keys.ENTER)
+    # time.sleep(10)
+    # print('отправил сообщение')
+    # print('end...')
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
